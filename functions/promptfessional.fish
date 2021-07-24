739b14bb73484203660b2975c6f9290b26e3f1c9
Ethan P <eth-p+git@hidden.email>
2021-07-23 18:09:10 -0700

    promptfessional: Clean up namespacing

--- functions/promptfessional.fish
+++ functions/promptfessional.fish
@@ -4,17 +4,147 @@
 # Documentation: https://github.com/eth-p/fish-promptfessional/tree/master/docs
 # Repository:    https://github.com/eth-p/fish-promptfessional
 # Issues:        https://github.com/eth-p/fish-promptfessional/issues
-#   
+#
 function promptfessional
-    __promptfessional_"$argv[1]" $argv[2..-1]
+    __promptfessional_fn_"$argv[1]" $argv[2..-1]
     return $status
 end
 
 # Ends the current section, printing its components.
-function __promptfessional_end
+function __promptfessional_fn_end --description "Ends a declared prompt section."
   __promptfessional_end_section
   set_color normal
 end
+
+# Declares the start of a prompt section.
+# This will implicitly end the previous prompt section, if there is one.
+function __promptfessional_fn_section --description "Declares the start of a prompt section."
+    argparse 'pattern=' 'delimiter=' 'current-color' -- $argv
+
+	# Argument: --get-color
+	if [ -n "$_flag_current_color" ]
+		printf "%s" "$__promptfessional_current_section_color"
+		return 0
+	end
+
+	# Set the default colors and end the previous section.
+	__promptfessional_theme --default dark
+    __promptfessional_end_section
+
+    # Default arguments.
+    [ -n "$_flag_pattern" ] || set _flag_pattern " %s "
+
+	# Initialize the section.
+    set -g __promptfessional_current_section $argv[1]
+    set -g __promptfessional_current_section_parts
+    set -g __promptfessional_current_section_pattern "$_flag_pattern"
+    set -g __promptfessional_current_section_delimiter "$_flag_delimiter"
+    set -g __promptfessional_current_section_color (promptfessional color "section.$argv[1]" --or "section")
+end
+
+# Displays a component.
+function __promptfessional_fn_show --description "Displays a prompt component."
+    if not functions "promptfessional_component_"$argv[1] &>/dev/null
+        echo "Unknown promptfessional component: $argv[1]" 1>&2
+        return 1
+    end
+
+    set -l text (promptfessional_component_"$argv[1]" $argv[2..-1])
+    if [ "$text" != "" ]
+      set -g __promptfessional_current_section_parts $__promptfessional_current_section_parts "$text"
+    end
+end
+
+# Declares the start of a prompt section.
+# This will implicitly end the previous prompt section, if there is one.
+function __promptfessional_fn_literal --description "Displays a prompt literal."
+	set -l literal "$argv[1]"
+	set literal (string replace -- "{arrow}" "$__promptfessional_section_arrow_symbol" "$literal")
+
+	printf "%s" "$literal"
+end
+
+# Enables a prompt setting.
+function __promptfessional_fn_enable --description "Enables a prompt feature."
+	switch $argv[1]
+		case "arrow"
+			set -g __promptfessional_section_arrow_symbol (printf "\uE0B0")
+			set -g __promptfessional_section_arrow_begin true
+
+		case "*"
+			echo "unknown prompfessional feature: $argv[1]" 1>&2
+			return 1
+	end
+end
+
+# Promptfessional: promptfessional color
+#
+# Get or set a prompt color.
+# Promptfessional uses its own system for fetching colors to use.
+# Under the hood, this uses fish's set_color function.
+#
+# Options:
+#   --set             :: Sets the color.
+#   --set-default     :: Sets the color if it's not already set.
+#   --only-foreground :: Only gets the foreground color.
+#   --only-background :: Only gets the background color.
+#   --only-attributes :: Only gets the attributes.
+#   --print           :: Prints the color name instead of the code.
+#   --or=COLOR_NAME   :: If the color is not set, use this color instead.
+#
+# Example:
+#   promptfessional color --set component.path --background=normal green   # Set the path to green.
+#   promptfessional color --set section.status --background=white
+function __promptfessional_fn_color --description "Gets or sets a prompt color."
+    argparse -i 'set' 'set-default' 'only-foreground' 'only-background' 'only-attributes' 'print' 'or=' -- $argv
+
+    set -l name (string replace --all -- '.' '_' $argv[1])
+    set -l value $argv[2..-1]
+	set -l color
+	eval "set color \$__promptfessional_colors__$name"
+    
+    # If --set-default is passed and the color is defined, return.
+    if [ -n "$_flag_set_default" ] && [ -n "$color" ]
+    	return
+	end
+
+	# If neither --set nor --set-default were passed, get the color.
+    if [ -z "$_flag_set" ] && [ -z "$_flag_set_default" ]
+        if [ -z "$color" ]
+        	if [ -n "$_flag_or" ]
+        		__promptfessional_color "$_flag_or" \
+        			$_flag_only_foreground $_flag_only_background $_flag_only_attributes
+        		return $status
+        	end
+        	return 1
+        end
+        
+		# If we're only trying to get the foreground/background/attributes, run it through
+		# the color extraction function.
+		if [ -n "$_flag_only_foreground$_flag_only_background$_flag_only_attributes" ]
+			set color (
+				__promptfessional_color_extract $color \
+					$_flag_only_foreground $_flag_only_background $_flag_only_attributes
+			)
+		end
+
+		# If we're only printing the color names, print them and exit early.
+		if [ -n "$_flag_print" ]
+			printf "%s\n" $color
+			return 0
+		end
+
+        set_color $color
+        return $result_status
+    end
+
+	# Otherwise, set the color.
+    set -g "__promptfessional_colors__$name" $value
+end
+
+# ----------------------------------------------------------------------------------------------------------------------
+# INTERNAL FUNCTIONS
+# ----------------------------------------------------------------------------------------------------------------------
 
 # INTERNAL:
 # Joins together all the rendered components and prints them.
@@ -82,132 +212,9 @@ function __promptfessional_end_section --description "Prints the current section
     set __promptfessional_current_section ''
 end
 
-# Declares the start of a prompt section.
-# This will implicitly end the previous prompt section, if there is one.
-function __promptfessional_section --description "Declares the start of a prompt section."
-    argparse 'pattern=' 'delimiter=' 'current-color' -- $argv
-
-	# Argument: --get-color
-	if [ -n "$_flag_current_color" ]
-		printf "%s" "$__promptfessional_current_section_color"
-		return 0
-	end
-
-	# Set the default colors and end the previous section.
-	__promptfessional_theme --default dark
-    __promptfessional_end_section
-
-    # Default arguments.
-    [ -n "$_flag_pattern" ] || set _flag_pattern " %s "
-
-	# Initialize the section.
-    set -g __promptfessional_current_section $argv[1]
-    set -g __promptfessional_current_section_parts
-    set -g __promptfessional_current_section_pattern "$_flag_pattern"
-    set -g __promptfessional_current_section_delimiter "$_flag_delimiter"
-    set -g __promptfessional_current_section_color (promptfessional color "section.$argv[1]" --or "section")
-end
-
-function __promptfessional_show
-    if not functions "promptfessional_component_"$argv[1] &>/dev/null
-        echo "Unknown promptfessional component: $argv[1]" 1>&2
-        return 1
-    end
-
-    set -l text (promptfessional_component_"$argv[1]" $argv[2..-1])
-    if [ "$text" != "" ]
-      set -g __promptfessional_current_section_parts $__promptfessional_current_section_parts "$text"
-    end
-end
-
-function __promptfessional_literal
-	set -l literal "$argv[1]"
-	set literal (string replace -- "{arrow}" "$__promptfessional_section_arrow_symbol" "$literal")
-
-	printf "%s" "$literal"
-end
-
-# Enables a prompt setting.
-function __promptfessional_enable
-	switch $argv[1]
-		case "arrow"
-			set -g __promptfessional_section_arrow_symbol (printf "\uE0B0")
-			set -g __promptfessional_section_arrow_begin true
-
-		case "*"
-			echo "unknown prompfessional feature: $argv[1]" 1>&2
-			return 1
-	end
-end
-
-# Promptfessional: promptfessional color
-#
-# Get or set a prompt color.
-# Promptfessional uses its own system for fetching colors to use.
-# Under the hood, this uses fish's set_color function.
-#
-# Options:
-#   --set             :: Sets the color.
-#   --set-default     :: Sets the color if it's not already set.
-#   --only-foreground :: Only gets the foreground color.
-#   --only-background :: Only gets the background color.
-#   --only-attributes :: Only gets the attributes.
-#   --print           :: Prints the color name instead of the code.
-#   --or=COLOR_NAME   :: If the color is not set, use this color instead.
-#
-# Example:
-#   promptfessional color --set component.path --background=normal green   # Set the path to green.
-#   promptfessional color --set section.status --background=white
-function __promptfessional_color
-    argparse -i 'set' 'set-default' 'only-foreground' 'only-background' 'only-attributes' 'print' 'or=' -- $argv
-
-    set -l name (string replace --all -- '.' '_' $argv[1])
-    set -l value $argv[2..-1]
-	set -l color
-	eval "set color \$__promptfessional_colors__$name"
-    
-    # If --set-default is passed and the color is defined, return.
-    if [ -n "$_flag_set_default" ] && [ -n "$color" ]
-    	return
-	end
-
-	# If neither --set nor --set-default were passed, get the color.
-    if [ -z "$_flag_set" ] && [ -z "$_flag_set_default" ]
-        if [ -z "$color" ]
-        	if [ -n "$_flag_or" ]
-        		__promptfessional_color "$_flag_or" \
-        			$_flag_only_foreground $_flag_only_background $_flag_only_attributes
-        		return $status
-        	end
-        	return 1
-        end
-        
-		# If we're only trying to get the foreground/background/attributes, run it through
-		# the color extraction function.
-		if [ -n "$_flag_only_foreground$_flag_only_background$_flag_only_attributes" ]
-			set color (
-				__promptfessional__color_extract $color \
-					$_flag_only_foreground $_flag_only_background $_flag_only_attributes
-			)
-		end
-
-		# If we're only printing the color names, print them and exit early.
-		if [ -n "$_flag_print" ]
-			printf "%s\n" $color
-			return 0
-		end
-
-        set_color $color
-        return $result_status
-    end
-
-	# Otherwise, set the color.
-    set -g "__promptfessional_colors__$name" $value
-end
-
 # INTERNAL:
 # Extracts set_color arguments and filters them.
-function __promptfessional__color_extract
+function __promptfessional_color_extract
 	argparse 'b/background=' 'o/bold' 'd/dim' 'i/italics' 'r/reverse' 'u/underline' \
 		'only-background' 'only-foreground' 'only-attributes' -- $argv
 	
@@ -236,4 +243,3 @@ function __promptfessional__color_extract
 		printf "%s\n" $_flag_bold $_flag_italics $_flag_dim $_flag_underline
 	end
 end
-
