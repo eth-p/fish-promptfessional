15fd25dbed72a0194ab6e58ff6037ceae43b161a
Ethan P <eth-p+git@hidden.email>
2022-05-25 12:07:10 -0700

    component_docker: Fix implementation issues
    - Do not show when unexported.
    - Specify contexts to hide.
    - Specify whether default context is hidden.

--- functions/promptfessional_component_docker.fish
+++ functions/promptfessional_component_docker.fish
@@ -2,23 +2,39 @@
 # Displays the current docker context.
 #
 # Options:
-#   --only-non-default    :: Only show if not the default context.
 #   --symbol              :: The symbol to display (default "").
+#   --hide-context [name] :: Hides the component if this context is used.
+#   --hide-default        :: Hides the component if the default context is used.
+#   --show-unexported     :: Shows the component even when DOCKER_CONTEXT is not exported.
 #
 # Colors:
 #   component.docker  :: Used for the docker context.
 function promptfessional_component_docker
-    argparse 'only-non-default' 'symbol=' -- $argv
-
+    argparse 'symbol=' 'show-unexported' 'hide-default' 'hide-context=+' -- $argv
     set -l current_context_name "$DOCKER_CONTEXT"
+    set -l current_context_display "$current_context_name"
 
-    # Handle default/unset Docker context.
-    if [ -z "$current_context_name" ]
-        if [ -n "$_flag_only_non_default" ]
+    if set --query DOCKER_CONTEXT
+        if not set --query --export DOCKER_CONTEXT && [ -z "$_flag_show_unexported" ]
+            # Don't show anything if the variable is set but unexported.
             return 0
         end
 
-        set current_context_name "default"
+        if [ -z "$DOCKER_CONTEXT" ]
+            set current_context_display "(empty)"
+        end
+    else
+        if [ -n "$_flag_hide_default" ]
+            # Don't show anything if '--hide-default' and not set.
+            return 0
+        end
+
+        set current_context_display "(default)"
+    end
+
+    # Return early if the context is specified in '--hide-context'.
+    if contains "$current_context_name" $_flag_hide_context
+        return 0
     end
 
     # Display.
@@ -27,6 +43,6 @@ function promptfessional_component_docker
         printf "%s " "$_flag_symbol"
     end
 
-    printf "%s" "$current_context_name"
+    printf "%s" "$current_context_display"
 end
 
